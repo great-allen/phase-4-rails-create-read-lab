d4d12d616c234c7cb49f30ceb09441d192a3f62e
QunFan <allenfan1012@gmail.com>
2023-02-08 10:18:55 +1100

    commit tree 495ade6ea4840f81eccf015a70b619a2cdb51a09

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,25 @@
+class PlantsController < ApplicationController
+
+    def create
+        plant = Plant.create(plant_params)
+        render json: plant, status: :created
+      end
+
+      def plant_params
+        params.permit(:name, :image, :price)
+      end
+
+      def index
+        plants=Plant.all 
+        render json: plants
+
+      end
+
+
+      def show
+        plant=Plant.find(params[:id])
+        render json: plant
+
+      end
+
+end
